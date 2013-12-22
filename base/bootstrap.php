99a0acc37089727b0477d3392ebec1d635f82923
Judd Vinet <judd@betsmartmedia.com>
2013-12-21 23:36:12 -0800

    lose the top-level subdir

--- base/bootstrap.php
+++ base/bootstrap.php
@@ -8,6 +8,12 @@ chdir(ROOT);
 require 'app/config/config.php';
 
 /*-----------------------------------------------------------------------
+ * Add Phrank to the include path
+ *-----------------------------------------------------------------------*/
+$phrank = realpath(dirname(__FILE__) . '/../');
+set_include_path(get_include_path() . PATH_SEPARATOR . $phrank);
+
+/*-----------------------------------------------------------------------
  * Setup Auto-loading
  *-----------------------------------------------------------------------*/
 require 'vendor/autoload.php';
