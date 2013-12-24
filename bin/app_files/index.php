b129c012cc0dca39dd195f3b8b051b11590dc3a8
Judd Vinet <judd@betsmartmedia.com>
2013-12-24 12:07:08 -0800

    fixed app_files to reflect path changes in reorg

--- bin/app_files/index.php
+++ bin/app_files/index.php
@@ -5,7 +5,10 @@
 
 define('ROOT', dirname(__FILE__) . '/..');
 
-include ROOT . '/phrank/base/bootstrap.php';
-include ROOT . '/phrank/base/bootstrap-web.php';
+// This path assumes you've installed Phrank via composer. If that's
+// not the case, then adjust these paths to point to the correct
+// location of the bootstrap files.
+include ROOT . '/vendor/bsm/phrank/Phrank/bootstrap.php';
+include ROOT . '/vendor/bsm/phrank/Phrank/bootstrap-web.php';
 
 ?>
