fdebd36f87dea12522c046a1a9683291a759167a
Judd Vinet <judd@betsmartmedia.com>
2014-01-02 16:15:14 -0800

    Model#load_data() renamed to populate() and can now accept a specific field list

--- Phrank/Base/Model.php
+++ Phrank/Base/Model.php
@@ -9,9 +9,12 @@ abstract class Model extends \Model
 	/**
 	 * Given a map of data, load fields into the model.
 	 */
-	public function load_data($data)
+	public function populate($data, $fields=null)
 	{
-		foreach($this->_fields as $field) {
+		if(is_null($fields)) {
+			$fields = $this->_fields;
+		}
+		foreach($fields as $field) {
 			if(isset($data[$field])) $this->{$field} = $data[$field];
 		}
 	}
